d0ea51c08ef8e8775711943202d828198e2e9937
Derek Hansen <derek@kaha.us>
2025-01-13 16:06:09 -0700

    remove tsserver

--- lua/extensions/mason.lua
+++ lua/extensions/mason.lua
@@ -12,7 +12,6 @@ mason.setup()
 mason_lspconfig.setup({
   ensure_installed = {
     "lua_ls",             -- LSP for Lua language
-    "tsserver",           -- LSP for Typescript and Javascript
     "emmet_ls",           -- LSP for Emmet (Vue, HTML, CSS)
     "cssls",              -- LSP for CSS
     "dockerls",           -- LSP for Docker
