522fbcd3df62cc3a96e083967a679efdc3ccce0b
Derek Hansen <derek@kaha.us>
2025-03-05 09:49:26 -0700

    use codellama

--- lua/extensions/CodeCompanion.lua
+++ lua/extensions/CodeCompanion.lua
@@ -4,7 +4,7 @@ require("codecompanion").setup({
 			return require("codecompanion.adapters").extend("ollama", {
 				schema = {
 					model = {
-						default = "llama3.2"
+						default = "codellama"
 					},
 				},
 			})
