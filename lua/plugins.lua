66b5f1f3886e3e977fd9dd9305d457bb0a95c5bf
Derek Hansen <derek@kaha.us>
2025-09-03 15:18:16 -0600

    more updates

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -94,7 +94,6 @@ return {
   -- Trouble {{{
   {
     "folke/trouble.nvim",
-    lazy = true,
     dependencies = "kyazdani42/nvim-web-devicons",
     config = function()
       require "extensions.trouble"
